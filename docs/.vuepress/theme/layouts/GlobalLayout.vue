f48c0fae0b4314fb2719f8a4d3f5cc61d6d7f58a
黄 <oncwnuLmJPSXaBSDc0zVXjfMvkCw@git.weixin.qq.com>
2021-03-09 20:05:49 +0800

    u

--- docs/.vuepress/theme/layouts/GlobalLayout.vue
+++ docs/.vuepress/theme/layouts/GlobalLayout.vue
@@ -46,12 +46,12 @@ export default {
 
 .content-wrapper
   padding 160px 15px 80px 15px
-  min-height calc(100vh - 80px - 60px - 160px)
+  min-height calc(100vh - 80px - 61px - 160px)
   max-width $contentWidth
   margin 0 auto
 
   @media (max-width: $MQMobile)
     &
       padding 100px 15px 20px 15px
-      min-height calc(100vh - 20px - 60px - 100px)
+      min-height calc(100vh - 20px - 61px - 100px)
 </style>
